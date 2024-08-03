1aab83eedb22d4720e1f82daca0fe31c13222f8f
asterf <migccast@gmail.com>
2024-08-03 17:20:48 -0500

    agregado rutas de asegurados y citas

--- initializers/loadEnv.go
+++ initializers/loadEnv.go
@@ -2,16 +2,15 @@ package initializers
 
 import (
 	"log"
-	"os"
 
 	"github.com/joho/godotenv"
 )
 
 func LoadEnvVariables() {
-	if os.Getenv("ENVIRONMENT") == "development" {
-		err := godotenv.Load()
-		if err != nil {
-			log.Fatal("Error al cargar archivo .env")
-		}
+	// if os.Getenv("ENVIRONMENT") == "development" {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error al cargar archivo .env")
 	}
+	// }
 }
